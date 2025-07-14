5b3250c76cae19367759eb8080beea92e262aa02
mukatirohit <rohanmukati2002@gmail.com>
2025-07-14 14:12:03 +0530

    model registry completed

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -127,7 +127,7 @@ def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
 
 
 def main():
-    mlflow.set_tracking_uri("http://ec2-52-91-160-21.compute-1.amazonaws.com:5000/")
+    mlflow.set_tracking_uri("http://ec2-3-106-54-30.ap-southeast-2.compute.amazonaws.com:5000/")
 
     mlflow.set_experiment('dvc-pipeline-runs')
     
--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("http://ec2-52-91-160-21.compute-1.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-3-106-54-30.ap-southeast-2.compute.amazonaws.com:5000/")
 
 
 # logging configuration
