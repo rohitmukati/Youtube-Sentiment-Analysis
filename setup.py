329f0181d0c1a0315b213db1cc9b61dd88bbfbe8
mukatirohit <rohanmukati2002@gmail.com>
2025-07-14 09:48:48 +0530

    folder strucuture created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Rohit',
+    author_email= 'rohanmukati@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
